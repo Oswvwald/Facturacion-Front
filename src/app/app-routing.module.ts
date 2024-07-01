e9e315693eef605460cb2c7b55655c49c6956a00
TCordova21 <124856715+TCordova21@users.noreply.github.com>
2024-07-01 16:55:40 -0500

    Login Creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,16 @@ import { ClientesViewComponent } from './clientes-view/clientes-view.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FacturaViewComponent } from './factura-view/factura-view.component';
 import { CreateInvoiceComponent } from './create-invoice/create-invoice.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 const routes: Routes = [
   {path: 'clientes-view', component: ClientesViewComponent},
   {path: 'sidebar', component: SidebarComponent},
   {path: 'facturas-view', component: FacturaViewComponent},
-  {path: 'create-invoice', component: CreateInvoiceComponent}
+  {path: 'create-invoice', component: CreateInvoiceComponent},
+  {path: 'Login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { FacturaViewComponent } from './factura-view/factura-view.component';
 import { CreateInvoiceComponent } from './create-invoice/create-invoice.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ClientDialogComponent,
     FacturaViewComponent,
     CreateInvoiceComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,

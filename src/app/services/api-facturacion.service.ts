09a212b63e4040e94bc3c5bcdb211ba95b4545de
Oswvwald <aovillarrealb@utn.edu.ec>
2024-07-16 01:21:59 -0500

    arreglo auditoria

--- src/app/services/api-facturacion.service.ts
+++ src/app/services/api-facturacion.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retryWhen, scan, delay } from 'rxjs/operators';
-import { tap, catchError, retry  } from 'rxjs/operators';
+import { tap, catchError, retry } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,24 +13,50 @@ export class ApiFacturacionService {
 
   url: string = 'http://45.70.13.48:3000/';
   private baseUrl = '/api'; // Utiliza el proxy configurado
-  private loginApi = 'https://api-modulo-seguridad.onrender.com/api/';
-  private auditApi = 'https://api-modulo-seguridad.onrender.com/api/auditoria'; 
+  private loginApi = 'https://api-modulo-seguridad.onrender.com/api/login_module';
+  private auditApi = 'https://api-modulo-seguridad.onrender.com/api/auditoria';
+  private userApi: string = 'https://api-modulo-seguridad.onrender.com/api/myuser';
+
+
   private functionalities: string[] = [];
-  private userloged : string;
+  private userloged: string;
 
   constructor(private api: HttpClient, private http: HttpClient) { }
 
   //Auditoria
-  private sendAudit(aud_usuario: string, aud_accion: string, aud_modulo: string, aud_funcionalidad: string, aud_observacion: string): Observable<any> {
-    const auditData = { aud_usuario, aud_accion, aud_modulo, aud_funcionalidad, aud_observacion };
-    return this.http.post(this.auditApi, auditData).pipe(
-      catchError(error => {
-        console.error('Error enviando auditoría:', error);
-        return throwError('Error enviando auditoría.');
-      })
-    );
+  private sendAudit(auditoriaData: {
+    aud_usuario: string,
+    aud_accion: string,
+    aud_modulo: string,
+    aud_funcionalidad: string,
+    aud_observacion: string
+  }): Observable<any> {
+    return this.http.post(this.auditApi, auditoriaData);
   }
+
+  private getAuthToken(): string {
+    return localStorage.getItem('access_token') || '';
+  }
+
+  getUserLoggedId(): void {
+      const authToken = this.getAuthToken();
+      const headers = { 'Authorization': `Bearer ${authToken}` };
   
+      this.http.get<{ usr_id: string }>(this.userApi, { headers }).pipe(
+        map(response => response.usr_id),
+        catchError(error => {
+          console.error('Error al obtener el ID del usuario logueado:', error);
+          return throwError(error); // Asegúrate de importar throwError de 'rxjs'
+        })
+      ).subscribe(usr_id => {
+        localStorage.setItem('user', usr_id);
+      });
+    }
+
+  get userLogged(): string {
+    return localStorage.getItem('user') || '';
+  }
+
 
   //Clientes
   //
@@ -44,25 +72,41 @@ export class ApiFacturacionService {
 
   //Crear un cliente
   createClient(cliente: any): Observable<any> {
+    this.userloged = this.userLogged;
     return this.api.post(this.url + 'clientes', cliente).pipe(
       tap(() => {
-        this.sendAudit(this.userloged, 'create', 'Facturación', 'clientes', 'Creación de cliente').subscribe();
-      }),
-      catchError(error => {
-        this.sendAudit(this.userloged, 'create', 'Facturación', 'clientes', `Error: ${error.message}`).subscribe();
-        return throwError(error);
+        this.sendAudit({
+          aud_usuario: this.userloged, // Cambiado de 'user' a 'aud_usuario'
+          aud_accion: 'create', // Cambiado de 'action' a 'aud_accion'
+          aud_modulo: 'Facturación', // Cambiado de 'module' a 'aud_modulo'
+          aud_funcionalidad: 'clientes', // Cambiado de 'entity' a 'aud_funcionalidad'
+          aud_observacion: `Creación de cliente con cédula: ${cliente.cedula}` // Cambiado de 'description' a 'aud_observacion'
+        }).subscribe();
       })
     );
   }
 
   //Editar un cliente
   editClient(cedula: string, cliente: any): Observable<any> {
+    this.userloged = this.userLogged;
     return this.api.put(this.url + 'clientes/' + cedula, cliente).pipe(
       tap(() => {
-        this.sendAudit(this.userloged, 'edit', 'Facturación', 'clientes', `Edición de cliente con cédula: ${cedula}`).subscribe();
+        this.sendAudit({
+          aud_usuario: this.userloged,
+          aud_accion: 'edit',
+          aud_modulo: 'Facturación',
+          aud_funcionalidad: 'clientes',
+          aud_observacion: `Edición de cliente con cédula: ${cedula}`
+        }).subscribe();
       }),
       catchError(error => {
-        this.sendAudit(this.userloged, 'edit', 'Facturación', 'clientes', `Error: ${error.message}`).subscribe();
+        this.sendAudit({
+          aud_usuario: this.userloged,
+          aud_accion: 'edit',
+          aud_modulo: 'Facturación',
+          aud_funcionalidad: 'clientes',
+          aud_observacion: `Error: ${error.message}`
+        }).subscribe();
         return throwError(error);
       })
     );
@@ -70,12 +114,25 @@ export class ApiFacturacionService {
 
   //Eliminar un cliente
   deleteClient(cedula: string): Observable<any> {
+    this.userloged = this.userLogged;
     return this.api.delete(this.url + 'clientes/' + cedula).pipe(
       tap(() => {
-        this.sendAudit(this.userloged, 'delete', 'Facturación', 'clientes', `Eliminación de cliente con cédula: ${cedula}`).subscribe();
+        this.sendAudit({
+          aud_usuario: this.userloged,
+          aud_accion: 'delete',
+          aud_modulo: 'Facturación',
+          aud_funcionalidad: 'clientes',
+          aud_observacion: `Eliminación de cliente con cédula: ${cedula}`
+        }).subscribe();
       }),
       catchError(error => {
-        this.sendAudit(this.userloged, 'delete', 'Facturación', 'clientes', `Error: ${error.message}`).subscribe();
+        this.sendAudit({
+          aud_usuario: this.userloged,
+          aud_accion: 'delete',
+          aud_modulo: 'Facturación',
+          aud_funcionalidad: 'clientes',
+          aud_observacion: `Error: ${error.message}`
+        }).subscribe();
         return throwError(error);
       })
     );
@@ -93,16 +150,49 @@ export class ApiFacturacionService {
   }
   //Crear un tipo de pago
   createTipoPago(tipoPago: any): Observable<any> {
-    return this.api.post(this.url + 'tipoPago', tipoPago);
+    this.userloged = this.userLogged;
+    return this.api.post(this.url + 'tipoPago', tipoPago).pipe(
+      tap(() => {
+        this.sendAudit({
+          aud_usuario: this.userloged, // Asume que `userloged` contiene el usuario actual
+          aud_accion: 'create', // Indica la acción de crear un nuevo tipo de pago
+          aud_modulo: 'Facturación', // El módulo al que pertenece esta acción
+          aud_funcionalidad: 'tipoPago', // La funcionalidad específica dentro del módulo
+          aud_observacion: `Creación de tipo de pago: ${tipoPago.nombre}` // Detalle de la acción realizada
+        }).subscribe();
+      })
+    );
   }
   //Editar un tipo de pago
   editTipoPago(id: string, tipoPago: any): Observable<any> {
-    return this.api.put(this.url + 'tipoPago/' + id, tipoPago);
+    this.userloged = this.userLogged;
+    return this.api.put(this.url + 'tipoPago/' + id, tipoPago).pipe(
+      tap(() => {
+        this.sendAudit({
+          aud_usuario: this.userloged, // Asume que `userloged` contiene el usuario actual
+          aud_accion: 'edit', // Indica la acción de editar un tipo de pago existente
+          aud_modulo: 'Facturación', // El módulo al que pertenece esta acción
+          aud_funcionalidad: 'tipoPago', // La funcionalidad específica dentro del módulo
+          aud_observacion: `Edición de tipo de pago con ID: ${id}` // Detalle de la acción realizada
+        }).subscribe();
+      })
+    );
   }
   //Eliminar un tipo de pago
   deleteTipoPago(id: string): Observable<any> {
-    return this.api.delete(this.url + 'tipoPago/' + id);
-  }
+    this.userloged = this.userLogged;
+      return this.api.delete(this.url + 'tipoPago/' + id).pipe(
+        tap(() => {
+          this.sendAudit({
+            aud_usuario: this.userloged, // Asume que `userloged` contiene el usuario actual
+            aud_accion: 'delete', // Indica la acción de eliminar un tipo de pago
+            aud_modulo: 'Facturación', // El módulo al que pertenece esta acción
+            aud_funcionalidad: 'tipoPago', // La funcionalidad específica dentro del módulo
+            aud_observacion: `Eliminación de tipo de pago con ID: ${id}` // Detalle de la acción realizada
+          }).subscribe();
+        })
+      );
+    }
 
   //Facturas
   //
@@ -118,7 +208,18 @@ export class ApiFacturacionService {
 
   //Crear una factura
   createFactura(factura: any): Observable<any> {
-    return this.api.post(this.url + 'facturas', factura);
+    this.userloged = this.userLogged;
+    return this.api.post(this.url + 'facturas', factura).pipe(
+      tap((response) => {
+        this.sendAudit({
+          aud_usuario: this.userloged, // Asume que `userloged` contiene el usuario actual
+          aud_accion: 'create', // Indica la acción de crear una factura
+          aud_modulo: 'Facturación', // El módulo al que pertenece esta acción
+          aud_funcionalidad: 'facturas', // La funcionalidad específica dentro del módulo
+          aud_observacion: `Creación de factura con ID: ${factura.factura_id}` // Detalle de la acción realizada, asumiendo que la respuesta incluye el ID de la factura creada
+        }).subscribe();
+      })
+    );
   }
 
   //Productos
@@ -145,7 +246,7 @@ export class ApiFacturacionService {
   //
   //Iniciar sesión
   login(username: string, password: string, moduleName: string): Observable<any> {
-    return this.http.post<any>(this.loginApi + 'login_module', {
+    return this.http.post<any>(this.loginApi, {
       usr_user: username,
       usr_password: password,
       mod_name: moduleName
@@ -155,18 +256,20 @@ export class ApiFacturacionService {
         const allowedRoles = ['FAC-ADMIN', 'FAC-FACTURADOR'];
         const userRole = response.Role;
         const hasAllowedRole = userRole.some((role: string) => allowedRoles.includes(role));
-  
+
         if (!hasAllowedRole) {
           // Si el usuario no tiene un rol permitido, lanza un error
           throw new Error('Rol no permitido. Acceso denegado.');
         }
-        
-        this.userloged = username;
+
         // Si el rol es permitido, guarda el token y las funcionalidades
         localStorage.setItem('access_token', response.access_token);
         localStorage.setItem('functionalities', JSON.stringify(response.functionalities));
         localStorage.setItem('role', JSON.stringify(response.Role));
         this.functionalities = response.functionalities;
+
+        this.getUserLoggedId(); // Obtiene el ID del usuario logueado
+
       }),
       catchError(error => {
         // Maneja errores de autenticación y otros errores
@@ -192,6 +295,8 @@ export class ApiFacturacionService {
   logout() {
     localStorage.removeItem('access_token');
     localStorage.removeItem('functionalities');
+    localStorage.removeItem('role');
+    localStorage.removeItem('user');
     this.functionalities = [];
   }
 
